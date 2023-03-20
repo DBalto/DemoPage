14dd0448a4c8b3f0510baba6aa3bfe40c61dcd22
Daniel Baltodano Mora <daniel.balto@hotmail.com>
2023-03-19 19:15:19 -0600

    Update Tests.js

--- Tests.js
+++ Tests.js
@@ -16,4 +16,47 @@ describe('Test Automation Page', () => {
     cy.get('#clear-btn').click();
     cy.get('#name-list').should('not.have.descendants', 'li');
   });
+
+  it('should display an error if no name is entered', () => {
+    cy.get('#add-btn').click();
+    cy.get('#name-error').should('have.text', 'Please enter a name.');
+  });
+
+  it('should not add a name if no name is entered', () => {
+    cy.get('#add-btn').click();
+    cy.get('#name-list').should('not.have.descendants', 'li');
+  });
+
+  it('should display an error if name is too long', () => {
+    const longName = 'This is a very long name that exceeds the maximum length allowed by the application.';
+    cy.get('#name').type(longName);
+    cy.get('#add-btn').click();
+    cy.get('#name-error').should('have.text', 'Name is too long.');
+  });
+
+  it('should not add a name if name is too long', () => {
+    const longName = 'This is a very long name that exceeds the maximum length allowed by the application.';
+    cy.get('#name').type(longName);
+    cy.get('#add-btn').click();
+    cy.get('#name-list').should('not.have.descendants', 'li');
+  });
+
+  it('should delete a name from the list', () => {
+    cy.get('#name').type('Charlie');
+    cy.get('#add-btn').click();
+    cy.get('#name-list li').should('have.length', 1);
+    cy.get('#name-list li button').click();
+    cy.get('#name-list').should('not.have.descendants', 'li');
+  });
+
+  it('should edit a name in the list', () => {
+    const newName = 'David';
+    cy.get('#name').type('Daniel');
+    cy.get('#add-btn').click();
+    cy.get('#name-list li').should('have.text', 'Daniel');
+    cy.get('#name-list li button').click();
+    cy.get('#edit-name').type(newName);
+    cy.get('#save-name').click();
+    cy.get('#name-list li').should('have.text', newName);
+  });
 });
